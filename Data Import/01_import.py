e26cf1047760a217884a0b5f82c73dc0db3b1ec5
Özkan <oakyuez@smail.uni-koeln.de>
2020-05-27 03:53:45 +0200

    a

--- Data Import/01_import.py
+++ Data Import/01_import.py
@@ -3,6 +3,8 @@ import pandas as pd
 # Import Nela Kaggle-News datasets and keep only columns regarding the title and source>
 nela = pd.read_csv("C:/Datasets/_all_features_NELA2017dataset.csv")
 
+= nela.loc[: ,[]]
+
 # Import Kaggle-News datasets, concat them and keep only columns like the title and source>
 kaggle_1 = pd.read_csv("C:/Datasets/articles1.csv")
 kaggle_2 = pd.read_csv("C:/Datasets/articles2.csv")
@@ -14,6 +16,9 @@ kaggle = kaggle.loc[: ,["title", "publication"]]
 
 # Concat both dataframes 
 news = pd.concat()
+
+# Label date with the political orientation based on the Media Rating by AllSides
+
 
 # Save dataframe as csv
 news.to_csv()
